446ddc6c80989b3f1c9469529d673e21ae157a23
nadav <nadav.kosov@gmail.com>
2017-12-24 21:51:48 +0200

    first commit to synagogue

---
+++ server/main.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var path = require("path");
+var serveStatic = require('static-server');
+app.use(express.static(__dirname + '/public'));
+app.get('/login', require('./login.html'));
+
+
+
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
+
